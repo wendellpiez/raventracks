36f2350922ef423ae47390933d8eba6c2ed8dbe2
Wendell Piez <wapiez@wendellpiez.com>
2026-07-19 15:18:41 -0400

    Convenience script runs a local web server

---
+++ docs/start-web-server.ps1
@@ -0,0 +1,8 @@
+# Calls Pode (web server for Powershell) - https://github.com/Badgerati/Pode
+# To install: https://github.com/Badgerati/Pode?tab=readme-ov-file#-install
+# Suggestion (as Administrator): Install-Module -Name Pode
+# There are some hoops to jump through
+# For example, see a page at http://localhost:8080/24MY_WRX_G9060BE_V2/HTML/contents/index.html
+
+Set-Location .
+Start-PodeStaticServer -FileBrowser -Port 7777
